24d6d2bf15de0a0fbf3b80cd1377f65973693c8f
Mandar Mane <maddy7350480@gmail.com>
2023-07-24 16:41:54 +0530

    Day Night Button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // My portfolio
- 
+
 // navbar
 
 document.querySelector('a').style.color = "orangered";
@@ -9,3 +9,22 @@ document.querySelector('a').style.color = "orangered";
 document.getElementById('myName').style.color = "orangered";
 // About
 
+// 
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+//  }
+
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var element = document.body;
+
+    // var text = document.getElementById("text");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+        element.classList.toggle("dark-mode");
+    }
+    else if (checkBox.checked == false) {
+        element.classList.toggle("light-mode");
+    }
+}
