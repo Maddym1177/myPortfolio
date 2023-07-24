86d5f4e2cdb5916ec662d8b0e387b08d82d4c7a2
Mandar Mane <maddy7350480@gmail.com>
2023-07-24 23:02:35 +0530

    24 july Last Commit With Button

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 
 // My portfolio
 
-// navbar
-
-document.querySelector('a').style.color = "orangered";
-
-// Home
-document.getElementById('myName').style.color = "orangered";
-// About
-
-// 
-// function myFunction() {
-//     var element = document.body;
-//     element.classList.toggle("dark-mode");
-//  }
-
+// button
 function myFunction() {
     var checkBox = document.getElementById("myCheck");
     var element = document.body;
 
-    // var text = document.getElementById("text");
-    // If the checkbox is checked, display the output text
     if (checkBox.checked == true) {
         element.classList.toggle("dark-mode");
     }
