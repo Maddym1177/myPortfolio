c28b7028bc2f69c635fbc2ff9568fe4a3db10ce9
Mandar Mane <maddy7350480@gmail.com>
2023-08-08 15:44:37 +0530

    Set

--- app.js
+++ app.js
@@ -2,14 +2,3 @@
 // My portfolio
 
 // button
-function myFunction() {
-    var checkBox = document.getElementById("myCheck");
-    var element = document.body;
-
-    if (checkBox.checked == true) {
-        element.classList.toggle("dark-mode");
-    }
-    else if (checkBox.checked == false) {
-        element.classList.toggle("light-mode");
-    }
-}
